f90644460cfc70c939d9dc1b828425681f407657
Lyndev-97 <layon.oliveira.lyn@gmail.com>
2023-02-06 15:09:37 -0300

    Service layer, component registration

--- src/main/java/com/DevlynBR/course/config/TestConfig.java
+++ src/main/java/com/DevlynBR/course/config/TestConfig.java
@@ -22,8 +22,9 @@ public class TestConfig implements CommandLineRunner{
 
 		User u1 = new User(2L, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
 		User u2 = new User(3L, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
+		User u3 = new User(4L, "Layon badass", "layon.badass@gmail.com", "964760771", "Devlyn$19");
 		
-		userRepository.saveAll(Arrays.asList(u1, u2));
+		userRepository.saveAll(Arrays.asList(u1, u2, u3));
 	}
 	
 }

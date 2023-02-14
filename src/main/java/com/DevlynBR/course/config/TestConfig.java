a5a7d3d6b95902d03d9708def582f7e90b50fad7
Lyndev-97 <layon.oliveira.lyn@gmail.com>
2023-02-14 09:12:36 -0300

    OrderStatus enum

--- src/main/java/com/DevlynBR/course/config/TestConfig.java
+++ src/main/java/com/DevlynBR/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.DevlynBR.course.entities.Order;
 import com.DevlynBR.course.entities.User;
+import com.DevlynBR.course.entities.enums.OrderStatus;
 import com.DevlynBR.course.repositories.OrderRepository;
 import com.DevlynBR.course.repositories.UserRepository;
 
@@ -25,13 +26,13 @@ public class TestConfig implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 
-		User u1 = new User(2L, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
-		User u2 = new User(3L, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
-		User u3 = new User(4L, "Layon badass", "layon.badass@gmail.com", "964760771", "Devlyn$19");
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
+		User u3 = new User(null, "Layon badass", "layon.badass@gmail.com", "964760771", "Devlyn$19");
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.DELIVERED, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.CANCELED, u3);
 		
 		
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));

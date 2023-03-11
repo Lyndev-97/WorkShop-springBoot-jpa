bc629b92e090f7abcf9e86780f59630d0ae082fa
Lyndev-97 <layon.oliveira.lyn@gmail.com>
2023-03-11 19:52:49 -0300

    OrderItem, many-to-many association with extra attributes Part2

--- src/main/java/com/DevlynBR/course/config/TestConfig.java
+++ src/main/java/com/DevlynBR/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.DevlynBR.course.entities.Category;
 import com.DevlynBR.course.entities.Order;
+import com.DevlynBR.course.entities.OrderItem;
 import com.DevlynBR.course.entities.Product;
 import com.DevlynBR.course.entities.User;
 import com.DevlynBR.course.entities.enums.OrderStatus;
 import com.DevlynBR.course.repositories.CategoryRepository;
+import com.DevlynBR.course.repositories.OrderItemRepository;
 import com.DevlynBR.course.repositories.OrderRepository;
 import com.DevlynBR.course.repositories.ProductRepository;
 import com.DevlynBR.course.repositories.UserRepository;
@@ -32,6 +34,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -71,6 +76,13 @@ public class TestConfig implements CommandLineRunner{
 		
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
 	
 }
--- src/main/java/com/DevlynBR/course/entities/OrderItem.java
+++ src/main/java/com/DevlynBR/course/entities/OrderItem.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.Objects;
 
 import com.DevlynBR.course.entities.pk.OrdemItemPk;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
@@ -12,18 +13,18 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_order_item")
-public class OrdemItem implements Serializable {
+public class OrderItem implements Serializable {
 	 private static final long serialVersionUID = 1L;
 
 	@EmbeddedId
-	private OrdemItemPk id;
+	private OrdemItemPk id = new OrdemItemPk();
 	private Integer quantity;
 	private Double price;
 	
-	public OrdemItem() {
+	public OrderItem() {
 	}
 
-	public OrdemItem(Order order, Product product, Integer quantity, Double price) {
+	public OrderItem(Order order, Product product, Integer quantity, Double price) {
 		super();
 		id.setOrder(order);
 		id.setProduct(product);
@@ -31,6 +32,7 @@ public class OrdemItem implements Serializable {
 		this.price = price;
 	}
 	
+	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
@@ -76,7 +78,7 @@ public class OrdemItem implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		OrdemItem other = (OrdemItem) obj;
+		OrderItem other = (OrderItem) obj;
 		return Objects.equals(id, other.id);
 	}
 	

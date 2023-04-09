206dc95fb38b189c685f68436e7c174d683add38
Lyndev-97 <layon.oliveira.lyn@gmail.com>
2023-04-08 22:34:56 -0300

    Exception handling - delete

---
+++ src/main/java/com/DevlynBR/course/services/exceptions/DatabaseException.java
@@ -0,0 +1,11 @@
+package com.DevlynBR.course.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public DatabaseException (String msg) {
+		super (msg);
+	}
+
+}

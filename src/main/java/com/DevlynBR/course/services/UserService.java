3815585b93c4284751efecd2f13395e22104dbe9
Lyndev-97 <layon.oliveira.lyn@gmail.com>
2023-04-08 22:49:11 -0300

    Exception handling - update

--- src/main/java/com/DevlynBR/course/services/UserService.java
+++ src/main/java/com/DevlynBR/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.DevlynBR.course.repositories.UserRepository;
 import com.DevlynBR.course.services.exceptions.DatabaseException;
 import com.DevlynBR.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -45,10 +47,13 @@ public class UserService {
 	}
 	
 	public User update (Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
-		
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 
